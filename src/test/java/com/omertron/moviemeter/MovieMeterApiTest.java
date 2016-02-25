63df38557b1cede12221dff4debed61d3a7619c3
Stuart Boston <omertron@gmail.com>
2016-02-25 19:12:25 +0000

    Sonar Fixes

--- src/test/java/com/omertron/moviemeter/MovieMeterApiTest.java
+++ src/test/java/com/omertron/moviemeter/MovieMeterApiTest.java
@@ -22,19 +22,15 @@ package com.omertron.moviemeter;
 import com.omertron.moviemeter.model.FilmInfo;
 import com.omertron.moviemeter.model.SearchResult;
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.junit.After;
-import org.junit.AfterClass;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -47,7 +43,7 @@ import org.slf4j.LoggerFactory;
 public class MovieMeterApiTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(MovieMeterApiTest.class);
-    private static final String PROP_FIlENAME = "testing.properties";
+    private static final String PROP_FILENAME = "testing.properties";
     private static String API_KEY;
 
     private static MovieMeterApi api;
@@ -55,45 +51,31 @@ public class MovieMeterApiTest {
 
     private static final int ID_IRON_MAN = 41789;
     private static final String IMDB_IRON_MAN = "tt0371746";
-
-    public MovieMeterApiTest() {
-    }
+    private static final String WRONG_MOVIE_ID_RETURNED = "Wrong Movie ID returned";
 
     @BeforeClass
     public static void setUpClass() throws MovieMeterException {
         // This must be the first statement in the beforeClass method
-        TestLogger.Configure();
+        TestLogger.configure();
         httpClient = HttpClients.createDefault();
 
         Properties props = new Properties();
-        File f = new File(PROP_FIlENAME);
+        File f = new File(PROP_FILENAME);
         if (f.exists()) {
-            LOG.info("Loading properties from '{}'", PROP_FIlENAME);
+            LOG.info("Loading properties from '{}'", PROP_FILENAME);
             TestLogger.loadProperties(props, f);
 
             API_KEY = props.getProperty("API_Key");
         } else {
-            LOG.info("Property file '{}' not found, creating dummy file.", PROP_FIlENAME);
+            LOG.info("Property file '{}' not found, creating dummy file.", PROP_FILENAME);
 
             props.setProperty("API_Key", "INSERT_YOUR_KEY_HERE");
 
             TestLogger.saveProperties(props, f, "Properties file for tests");
-            fail("Failed to get key information from properties file '" + PROP_FIlENAME + "'");
+            fail("Failed to get key information from properties file '" + PROP_FILENAME + "'");
         }
 
         api = new MovieMeterApi(API_KEY, httpClient);
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws IOException {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
     }
 
     /**
@@ -102,15 +84,15 @@ public class MovieMeterApiTest {
      * @throws com.omertron.moviemeter.MovieMeterException
      */
     @Test
-    public void testGetFilm_String() throws MovieMeterException {
+    public void testGetFilmString() throws MovieMeterException {
         LOG.info("getFilm (IMDB ID)");
 
         // Search for Iron Man
         FilmInfo result = api.getFilm(IMDB_IRON_MAN);
 
         assertNotNull("No movie returned", result);
-        assertEquals("Wrong Movie ID returned", ID_IRON_MAN, result.getId());
-        assertEquals("Wrong Movie ID returned", IMDB_IRON_MAN, result.getImdbId());
+        assertEquals(WRONG_MOVIE_ID_RETURNED, ID_IRON_MAN, result.getId());
+        assertEquals(WRONG_MOVIE_ID_RETURNED, IMDB_IRON_MAN, result.getImdbId());
     }
 
     /**
@@ -119,15 +101,15 @@ public class MovieMeterApiTest {
      * @throws com.omertron.moviemeter.MovieMeterException
      */
     @Test
-    public void testGetFilm_int() throws MovieMeterException {
+    public void testGetFilmInt() throws MovieMeterException {
         LOG.info("getFilm (MovieMeter ID)");
 
         // Search for Iron Man
         FilmInfo result = api.getFilm(ID_IRON_MAN);
 
         assertNotNull("No movie returned", result);
-        assertEquals("Wrong Movie ID returned", ID_IRON_MAN, result.getId());
-        assertEquals("Wrong Movie ID returned", IMDB_IRON_MAN, result.getImdbId());
+        assertEquals(WRONG_MOVIE_ID_RETURNED, ID_IRON_MAN, result.getId());
+        assertEquals(WRONG_MOVIE_ID_RETURNED, IMDB_IRON_MAN, result.getImdbId());
     }
 
     /**
